32cee5ab4ce072f8faedf5a0f430e299a5937599
feldt <cfeld20@student.aau.dk>
2022-08-16 15:19:35 +0200

    Testing out some stuff

--- main.py
+++ main.py
@@ -1,41 +1,68 @@
-
-# Press the green button in the gutter to run the script.
-
-# This guard is used to ensure that the file is not executed on import time
 import os
+import pandas as pd
 
-import pandas as pandas
-
+# Få data i et bedre format
+# For each student folder: in final and both midterm folders: read TEMP.csv and EDA.csv
+# Visualiser data enkeltvis
+# Make folder to save the visualisations in (REMEMBER TO SAVE)
 
 class Midterm:
-    tempData:list
-    edaData:list
+    tempData: dict
+    edaData: dict
+    def __init__(self, temp, eda):
+        self.tempData = temp
+        self.edaData = eda
+
+class Final:
+    tempData: list
+    edaData: list
+    def __init__(self, temp, eda):
+        self.tempData = temp
+        self.edaData = eda
 
 class Student:
     midterm: Midterm
+    final: Final
+    def __init__(self, student, midterm, final):
+        self.midterm =
+        self.final =
 
 if __name__ == '__main__':
-
-    # Få data i et bedre format
-        # For each student folder: in final and both midterm folders: read TEMP.csv and EDA.csv
-    # Visualiser data enkeltvis
-        # Make folder to save the visualisations in (REMEMBER TO SAVE)
-    x = [x[0] for x in os.walk(os.getcwd())]
 
     dataPath = os.path.join(os.getcwd(), "data")
     student_folders = [f.path for f in os.scandir(dataPath) if f.is_dir()]
 
-    data: dict[str, Student] = dict()
+    tempFinal = []
+    tempMidterm1 = []
+    tempMidterm2 = []
+    edaFinal = []
+    edaMidterm1 = []
+    edaMidterm2 = []
 
-    temperature = []
-    for folder in student_folders:
-        pathTemp = folder + '\\final\TEMP.csv'
-        temperature.append(pandas.read_csv(pathTemp))
-        data.get(folder).midterm.tempData.append(pandas.read_csv(pathTemp))
+    for student in student_folders:
+        pathTempFinal = os.path.join(student, 'final\TEMP.csv')
+        pathTempMidterm1 = os.path.join(student, 'Midterm 1\TEMP.csv')
+        pathTempMidterm2 = os.path.join(student, 'Midterm 2\TEMP.csv')
+        tempFinal.append(pd.read_csv(pathTempFinal))
+        tempMidterm1.append(pd.read_csv(pathTempMidterm1))
+        tempMidterm2.append(pd.read_csv(pathTempMidterm2))
 
-    dict.get('').
+        pathEDAFinal = os.path.join(student, 'final\EDA.csv')
+        pathEDAMidterm1 = os.path.join(student, 'Midterm 1\EDA.csv')
+        pathEDAMidterm2 = os.path.join(student, 'Midterm 2\EDA.csv')
+        edaFinal.append(pd.read_csv(pathEDAFinal))
+        edaMidterm1.append(pd.read_csv(pathEDAMidterm1))
+        edaMidterm2.append(pd.read_csv(pathEDAMidterm2))
 
-    print(temperature)
+    tempMidterm = {'midterm1': tempMidterm1, 'midterm2': tempMidterm2}
+    edaMidterm = {'midterm1': edaMidterm1, 'midterm2': edaMidterm2}
+    midterm = Midterm(tempMidterm, edaMidterm)
+
+    final = Final(tempFinal, edaFinal)
+
+    # data.get(folder).midterm.tempData.append(pandas.read_csv(pathTemp))
+    #dict.get('').
+    #data: dict[str, Student] = dict()
 
 
     # start by making a simple dataclass (google it) for some of the data and analyse that part of the data.
